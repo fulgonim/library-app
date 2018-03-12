4ebf4c6760d15449448b8650750d7421e0acd426
Max Fulgoni <fulgonim@gmail.com>
2018-03-12 12:46:31 +0800

    imports added to test file

---
+++ test/test-library-app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+const faker = require('faker');
+
+// configure chai to use 'expect' syntax
+const expect = chai.expect;
+
+
+
